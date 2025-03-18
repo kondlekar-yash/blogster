c67778688ca2cddec0c3941bb9e52efee23381a2
kondlekaryash <yashk9100@gmail.com>
2025-03-18 15:39:28 +1100

    moved mongo conn to env file

--- database/db.js
+++ database/db.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://blogster:bHQnqpqXTYF4oj0Y@blogs.vugsi.mongodb.net/"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
